01e2b6e71a5b6bf263fcb0b9765b3d8858609ad7
Abdelouafi TAHIRI <tahiriabdelouafi@gmail.com>
2024-02-02 15:27:25 +0100

    Initialiser une api (gestionnaire de stock)  spring boot

---
+++ api/src/test/java/com/abdel/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdel.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
